ed63768549fc8ee80ca951915387776298f2989c
Denis Zemlianskii <denya_mc43@mail.ru>
2023-07-30 15:25:59 +0300

    change names

--- js/index.js
+++ js/index.js
@@ -8,16 +8,15 @@ new Swiper('.swiper', {
     breakpoints: {
         320: {
             enabled: true,
-            slidesPerView: '1.25',
+            slidesPerView: 'auto',
+            spaceBetween: 16,
         },
         768: {
             enabled: false,
             slidesPerView: 'auto',
+            spaceBetween: 0,
         },
-        1120: {
-            enabled: false,
-            slidesPerView: 'auto',
-        }
+        
     }
 });
 
@@ -26,30 +25,18 @@ const closeButtonPopup = document.querySelector('.popup-button__hide');
 const brandButtonHide = document.querySelectorAll('.brand-button__hide')
 const brandButtonWidth = document.querySelectorAll('.brand-button__hide-width')
 
-
-
-openButtonPopup.addEventListener('click', function () {
-    openButtonPopup.classList.add('popup-button__hide');
-    closeButtonPopup.classList.remove('popup-button__hide');
+function togglePopup() {
+    openButtonPopup.classList.toggle('popup-button__hide');
+    closeButtonPopup.classList.toggle('popup-button__hide');
     
     for (let i = 0; i < brandButtonHide.length; i++) {
-        brandButtonHide[i].classList.remove('brand-button__hide');
+        brandButtonHide[i].classList.toggle('brand-button__hide');
     }
 
     for (let i = 0; i < brandButtonWidth.length; i++) {
-        brandButtonWidth[i].classList.remove('brand-button__hide-width')
+        brandButtonWidth[i].classList.toggle('brand-button__hide-width')
     }
-});
+}
 
-closeButtonPopup.addEventListener('click', function () {
-    closeButtonPopup.classList.add('popup-button__hide');
-    openButtonPopup.classList.remove('popup-button__hide');
-
-    for (let i = 0; i < brandButtonHide.length; i++) {
-        brandButtonHide[i].classList.add('brand-button__hide');
-    }
-
-    for (let i = 0; i < brandButtonWidth.length; i++) {
-        brandButtonWidth[i].classList.add('brand-button__hide-width')
-    }
-});
+openButtonPopup.addEventListener('click', togglePopup);
+closeButtonPopup.addEventListener('click', togglePopup);
